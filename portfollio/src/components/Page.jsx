5ccc29dad4a793972e7ffa37336b5ea92762c1aa
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-10-05 00:20:25 +0530

    Second page of about me create

--- portfollio/src/components/Page.jsx
+++ portfollio/src/components/Page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Page.css"
 import Navpan from './Navpan'
 import Page1 from './pages/Page1'
+import Page2 from './pages/Page2'
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
     <div className="main-page">
     <Navpan></Navpan>
     <Page1></Page1>
+    <Page2></Page2>
     </div>
     </>
   )
--- portfollio/src/components/pages/Page2.jsx
+++ portfollio/src/components/pages/Page2.jsx
@@ -2,8 +2,57 @@ import React from 'react'
 import "./Page2.css"
 import "../syle.css"
 
+import { FaFileDownload } from "react-icons/fa";
+
+
+import sih2024 from "/images/sih2024.jpg"
+import hackindore from "/images/hackindore.jpg"
+import formal from "/images/formal.jpg"
+import hackindoregrp from "/images/hackindoregrp.jpg"
+
 export default function Page2() {
-  return (
-    <div>Page2</div>
-  )
+    return (
+        <>
+            <div className="page-2">
+
+                <div className="pg-2-heading">
+                    About Me
+                </div>
+
+                <div className="abt-me-container">
+                    <div className="abt-me-photo">
+                        <img src={hackindoregrp} alt="" />
+                    </div>
+                    <div className="abt-me-desc">
+                        <h1>I'm Mohit Soni</h1>
+                        <div className="abt-me-caption">
+                            As a 3rd-year engineering student at IET DAVV, I’m passionate about leveraging technologies like Data Structures & Algorithms, Java, DBMS, SQL, and the MERN stack to develop working prototypes. I had participated in tech competitions, hackathons, and workshops, focusing on web and mobile app development, and programming solutions.
+                        </div>
+                        {/* ---------Feture------ */}
+                        <div className="pg-2-feature-cont">
+                            {/* feature-1----- */}
+                            <div className="pg2-fea">
+                            <div style={{fontSize:"1.6rem" , fontWeight:"500"}}>300+</div>
+                            <div style={{fontSize:"1.1rem" , fontWeight:"500" , color:"rgba(0,0,0,0.4)"}}>Years of Experience</div>
+                                
+                            </div>
+                            <div className="pg2-fea">
+                            <div style={{fontSize:"1.6rem" , fontWeight:"500"}}>300+</div>
+                            <div style={{fontSize:"1.1rem" , fontWeight:"500" , color:"rgba(0,0,0,0.4)"}}>Years of Experience</div>
+                            
+                            </div>
+                            <div className="pg2-fea">
+                            <div style={{fontSize:"1.6rem" , fontWeight:"500"}}>300+</div>
+                            <div style={{fontSize:"1.1rem" , fontWeight:"500" , color:"rgba(0,0,0,0.4)"}}>Years of Experience</div>
+                            </div>
+                        </div>
+
+                        <div className="down-cv-btn">Download CV &nbsp;<FaFileDownload size={21} />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
 }
