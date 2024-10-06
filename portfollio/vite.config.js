86079228ca46dfa029f26a4a58bcca7e31b3155f
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-10-06 22:49:23 +0530

    View project over Same network (wifi)

--- portfollio/vite.config.js
+++ portfollio/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // Or use "0.0.0.0" to bind to all interfaces
+    port: 3000,   // You can specify any port you prefer
+  },
 })
